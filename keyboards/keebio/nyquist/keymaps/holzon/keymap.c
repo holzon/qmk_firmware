e6cfbbf1955f3638ceafd69214bd10a77ec05afa
Jens Olsson <jens@himiltungl.com>
2021-11-28 16:45:22 +0100

    moved over features from planck keymap

--- keyboards/keebio/nyquist/keymaps/holzon/keymap.c
+++ keyboards/keebio/nyquist/keymaps/holzon/keymap.c
@@ -1,21 +1,49 @@
 #include QMK_KEYBOARD_H
 
+#include "quantum/keymap_extras/keymap_swedish.h"
+
 enum layer_names {
     _QWERTY,
     _LOWER,
     _RAISE,
     _FUNCTION,
     _ADJUST,
+    _MOVE,
+    _NUMPAD
 };
 
-enum planck_keycodes {
-  LOWER = SAFE_RANGE,
-  RAISE
-};
+// enum planck_keycodes {
+//   LOWER = SAFE_RANGE,
+//   RAISE
+// };
 
 // Defines for task manager and such
 #define CALTDEL LCTL(LALT(KC_DEL))
 #define TSKMGR LCTL(LSFT(KC_ESC))
+
+#define LOWER MO(_LOWER)
+#define RAISE MO(_RAISE)
+#define MOVE MO(_MOVE)
+#define NUMPAD MO(_NUMPAD)
+
+// Tap Dance declarations
+enum {
+    TD_ESC_NUMPAD,
+    TD_ARNG_ADIA,
+    TD_QUOT_ARNG
+};
+
+// `finished` and `reset` functions for each tapdance keycode
+void esc_numpad_finished(qk_tap_dance_state_t *state, void *user_data);
+void esc_numpad_reset(qk_tap_dance_state_t *state, void *user_data);
+
+// Tap Dance definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for Escape, twice for Caps Lock
+    [TD_ESC_NUMPAD] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, esc_numpad_finished, esc_numpad_reset),
+    [TD_ARNG_ADIA] = ACTION_TAP_DANCE_DOUBLE(SE_ADIA, SE_ARNG),
+    [TD_QUOT_ARNG] = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, KC_LBRC)
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -32,9 +60,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = LAYOUT_ortho_4x12(
   KC_TAB,        KC_Q,   KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
-  MO(_FUNCTION), KC_A,   KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_ENT,
-  OSM(MOD_LSFT), KC_Z,   KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
-  KC_LCTL,       KC_GRV, KC_LGUI, KC_LALT, LOWER, KC_SPC, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  TD(TD_ESC_NUMPAD),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, TD(TD_QUOT_ARNG),
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
+  LCTL_T(KC_LBRACKET), KC_LGUI, KC_LALT, MOVE, LOWER,   KC_SPC,  KC_SPC,  RAISE, KC_APPLICATION, KC_RALT, KC_RGUI, RCTL_T(KC_RBRACKET)
 ),
 
 /* Lower
@@ -49,10 +77,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_ortho_4x12(
-  KC_ESC,  KC_EXLM,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
-  _______, KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______,
-  _______, KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_MUTE, _______, KC_PIPE,
-  _______, _______,  _______, _______, _______, KC_BSPC, KC_BSPC, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+  KC_TILD,  KC_EXLM,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+  KC_DEL, KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
+  _______, KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_MUTE, _______, _______,
+  KC_LEFT_PAREN, KC_LBRACKET, KC_LEFT_CURLY_BRACE, KC_LEFT_ANGLE_BRACKET, _______, _______, _______, _______, KC_RIGHT_ANGLE_BRACKET, KC_RIGHT_CURLY_BRACE, KC_RBRACKET, KC_RIGHT_PAREN
+
 ),
 
 /* Raise
@@ -67,10 +96,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_ortho_4x12(
-  KC_ESC,  KC_1,    KC_2,  KC_3,   KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-  _______, KC_4,    KC_5,  KC_6,   KC_PLUS, KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
-  KC_ENT,  KC_7,    KC_8,  KC_9,   KC_MINS, KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_MUTE, _______, KC_BSLS,
-  _______, KC_COMM, KC_0,  KC_DOT, _______, KC_BSPC, KC_BSPC, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
+    KC_LEFT_PAREN, KC_LBRACKET, KC_LEFT_CURLY_BRACE, KC_LEFT_ANGLE_BRACKET, _______, _______, _______, _______, KC_RIGHT_ANGLE_BRACKET, KC_RIGHT_CURLY_BRACE, KC_RBRACKET, KC_RIGHT_PAREN
 ),
 
 /* Adjust (Lower + Raise)
@@ -86,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] = LAYOUT_ortho_4x12(
   TSKMGR,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, CALTDEL,
-  _______, _______, _______, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______, _______, _______, _______, _______,
+  KC_SYSTEM_SLEEP, _______, _______, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______, _______, _______, _______, _______,
   _______,  MUV_DE, MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET
 ),
@@ -107,33 +136,72 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
   KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+),
+
+[_MOVE] = LAYOUT_ortho_4x12(
+    KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9,  KC_F10, KC_F11, KC_F12,
+    _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT, SE_ODIA, TD(TD_ARNG_ADIA),
+    _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
+),
+
+[_NUMPAD] = LAYOUT_ortho_4x12(
+    _______, _______, _______, _______, _______, _______, _______, KC_NUMLOCK, KC_KP_7,  KC_KP_8, KC_KP_9, KC_KP_MINUS,
+    _______, _______, _______, _______, _______, _______, _______, KC_INSERT, KC_KP_4,  KC_KP_5, KC_KP_6, KC_KP_PLUS,
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_KP_1,  KC_KP_2, KC_KP_3, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_KP_0,  _______, KC_KP_DOT, _______
 )
 
 };
 
+// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//   switch (keycode) {
+//     case LOWER:
+//       if (record->event.pressed) {
+//         layer_on(_LOWER);
+//         update_tri_layer(_LOWER, _RAISE, _ADJUST);
+//       } else {
+//         layer_off(_LOWER);
+//         update_tri_layer(_LOWER, _RAISE, _ADJUST);
+//       }
+//       return false;
+//       break;
+//     case RAISE:
+//       if (record->event.pressed) {
+//         layer_on(_RAISE);
+//         update_tri_layer(_LOWER, _RAISE, _ADJUST);
+//       } else {
+//         layer_off(_RAISE);
+//         update_tri_layer(_LOWER, _RAISE, _ADJUST);
+//       }
+//       return false;
+//       break;
+//   }
+//   return true;
+// }
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case LOWER:
-      if (record->event.pressed) {
-        layer_on(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case RAISE:
-      if (record->event.pressed) {
-        layer_on(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
+layer_state_t layer_state_set_user(layer_state_t state) {
+  state = update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+  return state;
+}
+
+void matrix_init_user (void) {
+  if (!(host_keyboard_leds() & (1<<USB_LED_NUM_LOCK))) {
+      register_code(KC_NUMLOCK);
+      unregister_code(KC_NUMLOCK);
   }
-  return true;
+}
+
+void esc_numpad_finished(qk_tap_dance_state_t *state, void *user_data) {
+  if (!state->pressed) {
+    tap_code(KC_ESC);
+  } else {
+    layer_on(_NUMPAD);
+  }
+}
+
+void esc_numpad_reset(qk_tap_dance_state_t *state, void *user_data) {
+  if (layer_state_is(_NUMPAD)) {
+    layer_off(_NUMPAD);
+  }
 }
--- keyboards/keebio/nyquist/keymaps/holzon/rules.mk
+++ keyboards/keebio/nyquist/keymaps/holzon/rules.mk
@@ -1,1 +1,3 @@
 RGBLIGHT_ENABLE = yes
+
+TAP_DANCE_ENABLE = yes
